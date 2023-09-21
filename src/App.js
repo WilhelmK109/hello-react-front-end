038377cc862e762f14d347d9d2be5de8ce06de63
Wilhelm Kamulunga <kamulungaw@gmail.com>
2023-09-21 17:11:32 +0200

    Add project components, reducer, and store

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import Greeting from './Greeting';
+import './App.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
